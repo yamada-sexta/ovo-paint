afed2339988cdfc96b8c1d3e8c73da552c7d6160
NaN Noda <nananannoda@gmail.com>
2023-05-12 17:09:13 -0500

    Update Debug.ts

--- src/Core/Debug.ts
+++ src/Core/Debug.ts
@@ -1,6 +1,7 @@
 import {DocNode} from "./Documents/DocNodes/DocNode";
 import {GroupNode} from "./Documents/DocNodes/GroupNode";
 
+
 export function printDocNodeTree(node: DocNode, depth: number = 0) {
     console.log("  ".repeat(depth) + " " + node.name + " [" + node.constructor.name + "]");
     if (node instanceof GroupNode) {
